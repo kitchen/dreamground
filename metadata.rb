26ca9bf2ba97736feedbcf8258ef8e39f5a710a3
Jeremy Kitchen <kitchen@kitchen.io>
2018-08-23 21:47:09 -0700

    metadata and LICENSE
    WTFPL

--- metadata.rb
+++ metadata.rb
@@ -1,24 +1,14 @@
 name 'dreamground'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures dreamground'
-long_description 'Installs/Configures dreamground'
+maintainer 'Jeremy Kitchen'
+maintainer_email 'kitchen@kitchen.io'
+license 'WTFPL'
+description 'my playground configuratororor'
+long_description 'I have a playground server, this configures it'
 version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/dreamground/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/dreamground'
-#
+issues_url 'https://github.com/kitchen/dreamground/issues'
+source_url 'https://github.com/kitchen/dreamground'
 
 depends 'users'
 depends 'chef-apt-docker'
